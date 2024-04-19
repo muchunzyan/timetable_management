1296ba1133b822790dacea6d507f95b213fd8985
Михаил Учунжян <msuchunzhyan@edu.hse.ru>
2024-04-19 15:23:52 +0300

    reservation_model.py _rec_name

--- custom-addons/timetable_management_module/models/reservation_model.py
+++ custom-addons/timetable_management_module/models/reservation_model.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class ReservationModel(models.Model):
     _name = "reservation_model"
     _description = "Reservation Model"
+    _rec_name = "discipline_id"
 
     discipline_id = fields.Many2one("discipline_model", string="Discipline")
     event_type_id = fields.Many2one("event_type_model", string="Event type")
