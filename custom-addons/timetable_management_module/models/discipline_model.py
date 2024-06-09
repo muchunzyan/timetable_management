fc05a21895696ec303a3801b9ee2ef8de60cadcd
Mikhail Uchunzhyan <muchunzyan@mail.ru>
2024-06-09 22:39:50 +0300

    filters in searches

--- custom-addons/timetable_management_module/models/discipline_model.py
+++ custom-addons/timetable_management_module/models/discipline_model.py
@@ -1,9 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class DisciplineModel(models.Model):
     _name = "discipline_model"
     _description = "Discipline Model"
+    _rec_name = "name"
 
-    name = fields.Char(string="Name")
-    professor_ids = fields.Many2many("res.users", string="Professors")
+    name = fields.Char("Discipline Name", required=True)
+    professor_ids = fields.Many2many('res.users', string="Professors", domain=lambda self: self._get_professor_domain())
+
+    @api.model
+    def _get_professor_domain(self):
+        # Reference the Professor group
+        professor_group = self.env.ref('student.group_professor')
+        return [('groups_id', 'in', professor_group.id)]
